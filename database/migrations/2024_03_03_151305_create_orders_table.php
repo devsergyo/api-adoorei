d7681c7198665c319e25996d32f1433e94dd8221
Sergyo Moreira <dev@selectasolutions.com.br>
2024-03-03 13:27:17 -0300

    Refactor: Adicionado default em 'amount' da tabela Order e removido timestamps da tabela pivot

--- database/migrations/2024_03_03_151305_create_orders_table.php
+++ database/migrations/2024_03_03_151305_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->decimal('amount',10,2);
+            $table->decimal('amount',10,2)->default(0);
             $table->enum('status', [
                 'created',
                 'canceled',
